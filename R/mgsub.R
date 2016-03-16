df6fccfc77079948d703674de8d16bfbbdd76294
Tyler <tyler.rinker@gmail.com>
2016-03-15 22:28:12 -0400

    updated replace_contractions

--- R/mgsub.R
+++ R/mgsub.R
@@ -50,6 +50,25 @@ function (x, pattern, replacement, leadspace = FALSE,
 }
 
 
-
+spaste <-
+function (terms, trailing = TRUE, leading = TRUE) {
+    if (leading) {
+        s1 <- " "
+    }     else {
+        s1 <- ""
+    }
+    if (trailing) {
+        s2 <- " "
+    } else {
+        s2 <- ""
+    }
+    pas <- function(x) paste0(s1, x, s2)
+    if (is.list(terms)) {
+        z <- lapply(terms, pas)
+    } else {
+        z <- pas(terms)
+    }
+    return(z)
+}
 
 
