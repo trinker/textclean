ac3b649ed6c75e4dc829b8a363ce234c93e1ac00
Tyler <tyler.rinker@gmail.com>
2016-03-17 22:22:15 -0400

    more fixes

---
+++ R/textclean-package.R
@@ -0,0 +1,28 @@
+#' Text Cleaning Tools
+#'
+#' Tools to clean and process text.
+#' @docType package
+#' @name textmod
+#' @aliases textmod package-textmod
+NULL
+
+#' Fictitious Classroom Dialogue
+#'
+#' A fictitious dataset useful for small demonstrations.
+#'
+#' @details
+#' \itemize{
+#'   \item person. Speaker
+#'   \item sex. Gender
+#'   \item adult. Dummy coded adult (0-no; 1-yes)
+#'   \item state. Statement (dialogue)
+#'   \item code. Dialogue coding scheme
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name DATA
+#' @usage data(DATA)
+#' @format A data frame with 11 rows and 5 variables
+NULL
+
