b7c2903ed13b340e137d41d9e96c434678153afa
Tyler <tyler.rinker@gmail.com>
2017-01-16 22:12:33 -0500

    forgot symbol arg

--- R/replace_html.R
+++ R/replace_html.R
@@ -21,6 +21,8 @@
 #' }
 #' 
 #' @param x  The text variable.
+#' @param symbol lofical.  If code{TRUE} the sybols are retained with appropriate
+#' replacments.  If \code{FALSE} they are removed.
 #' @param \ldots Ignored.
 #' @return Returns a vector with HTML markup replaced.
 #' @keywords html
